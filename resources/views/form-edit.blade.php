1298c95bf9dd0ac8f5cbdff291706fb42b81a4a3
soffanderson <s.btudz@gmail.com>
2022-08-04 20:47:45 +0800

    Fix footer edit-data

--- resources/views/form-edit.blade.php
+++ resources/views/form-edit.blade.php
@@ -73,23 +73,24 @@
                 class="text-black bg-slate-100 hover:bg-slate-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">Cancel</a>
         </div>
     </form>
-    @endsection
+</div>
+@endsection
 
-    {{--
-    <a href="/">
-        << Back</a>
-            <form action="" method="POST">
-                @csrf
-                <h2>Edit Product</h2><br>
-                <input type="text" name="name" placeholder="Product Name" value="{{ $product->name }}"><br>
-                <textarea name="description" id="" cols="30" rows="10"
-                    placeholder="Deskripsi">{{ $product->description }}</textarea>
-                <br>
-                <input type="number" name="stock" placeholder="stock" value="{{ $product->stock }}">
-                <br>
-                <input type="number" name="price" placeholder="price" value="{{ $product->price }}">
-                <br>
-                <input type="file" name="image" placeholder="image">
-                <br>
-                <input type="submit" value="Save Data">
-            </form> --}}
+{{--
+<a href="/">
+    << Back</a>
+        <form action="" method="POST">
+            @csrf
+            <h2>Edit Product</h2><br>
+            <input type="text" name="name" placeholder="Product Name" value="{{ $product->name }}"><br>
+            <textarea name="description" id="" cols="30" rows="10"
+                placeholder="Deskripsi">{{ $product->description }}</textarea>
+            <br>
+            <input type="number" name="stock" placeholder="stock" value="{{ $product->stock }}">
+            <br>
+            <input type="number" name="price" placeholder="price" value="{{ $product->price }}">
+            <br>
+            <input type="file" name="image" placeholder="image">
+            <br>
+            <input type="submit" value="Save Data">
+        </form> --}}
